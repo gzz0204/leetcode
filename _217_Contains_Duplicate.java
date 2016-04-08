75500d55d984bb5e4bf4f7fc38aa5937d4bfc09c
Jieao Zhu <zhujieao@gmail.com>
2016-04-07 22:38:05 -0400

    217

---
+++ _217_Contains_Duplicate.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(hm.get(nums[i]) != null){
+                return true;
+            }else{
+                hm.put(nums[i], 1);
+            }
+        }
+        return false;
+    }
+}
