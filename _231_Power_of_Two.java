3987ae04b29a2e267aff6b8f1bb0503ea15c1ff5
Jieao Zhu <zhujieao@gmail.com>
2016-04-09 16:19:08 -0400

    231

---
+++ _231_Power_of_Two.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        while(n > 1){
+            if(n % 2 != 0){
+                return false;
+            }else{
+                n /= 2;
+            }
+        }
+        return n == 1;
+    }
+}
