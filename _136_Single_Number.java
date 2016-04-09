b88a7ea08d479b463a7f577a3a7dfc6890c6bb52
Jieao Zhu <zhujieao@gmail.com>
2016-04-09 16:44:48 -0400

    136, hash map  solution

--- _136_Single_Number.java
+++ _136_Single_Number.java
@@ -1,6 +1,28 @@
 public class Solution {
     public int singleNumber(int[] nums) {
         // xor
+        return singleNumberHashMap(nums);
+    }
+
+    public int singleNumberHashMap(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int num : nums){
+            if(map.get(num) == null){
+                map.put(num, false);
+            }else{
+                map.put(num, true);
+            }
+        }
+        for(Integer num : map.keySet()){
+            if(map.get(num) == false){
+                return num;
+            }
+        }
+        return -1;
+    }
+
+    public int singleNumberBitManipulation(int[] nums) {
+        // xor
         int result = 0;
         for(int num : nums){
             result ^= num;
