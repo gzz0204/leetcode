9c5927601fcbb4a31daa5385a6b92515198de3c9
Jieao Zhu <zhujieao@gmail.com>
2016-04-11 21:10:38 -0400

    078, recursive

--- _078_Subsets.java
+++ _078_Subsets.java
@@ -18,4 +18,32 @@ public class Solution {
         }
         return res;
     }
+
+
+    public List<List<Integer>> subsets2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums){
+            set.add(i);
+        }
+        int len = set.size();
+        int[] elements = new int[len];
+        int index = 0;
+        for(int element : set){
+            elements[index] = element;
+            index ++;
+        }
+        Arrays.sort(elements);
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        subsetHelper(res, elements, new LinkedList<Integer>(), -1);
+        return res;
+    }
+
+    private void subsetHelper(LinkedList<List<Integer>> res, int[] nums, LinkedList<Integer> prevs, int from){
+        res.addLast((LinkedList<Integer>)prevs.clone());
+        for(int i = from + 1; i < nums.length; i++){
+            prevs.addLast(nums[i]);
+            subsetHelper(res, nums, prevs, i);
+            prevs.removeLast();
+        }
+    }
 }
